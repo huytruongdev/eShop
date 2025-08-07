08a13e217b0855569f8bd8d275e9c70632c1e963
huytruongdev <kzyo5962@gmail.com>
2025-08-08 01:32:36 +0700

    setup Basket Service

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using BuildingBlocks.CQRS;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Basket.API.Models;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
