5e8af2501dd10e9a7abbe9ba1c8a0328276b470b
huytruongdev <kzyo5962@gmail.com>
2025-08-23 00:31:10 +0700

    OrderingService: implement CQRS

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+public class OrderCreatedEventHandler
+    (ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Domain Event handled: {domainEvent.GetType().Name}");
+        await Task.CompletedTask;
+        
+    }
+}
